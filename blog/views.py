0cc8c2b1e84b6bb6c5bedc2a4393ba3ee5c4bd20
Haimana Qare <78535674+haimy254@users.noreply.github.com>
2023-06-21 06:40:15 +0300

    changes at home template

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id'] #displays according to the order, the last one to be displayed will be at the top
 
 class ArticleView(DetailView):
     model = Post
