20eef7b0ef16bef97ca1d8625382bf59d275c7d2
Haimana Qare <78535674+haimy254@users.noreply.github.com>
2023-06-23 06:44:31 +0300

    added date in the model

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    ordering = ['-id'] #displays according to the order, the last one to be displayed will be at the top
+    ordering = ['-post_date'] #displays according to the order, the last one to be displayed will be at the top
 
 class ArticleView(DetailView):
     model = Post
